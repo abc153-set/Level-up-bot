4048f00151fbe72ccdbf26363b1fc97a12ffc46f
abc153-set <km7124298@gmail.com>
2025-06-05 10:27:15 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,8 +13,7 @@ db = firestore.client()
 import telebot
 
 # Your Telegram Bot Token
-BOT_TOKEN ="8125526527:AAEd2PVmfb1CATBCxKHzgXec6
-DXNI5ZqB1o"
+BOT_TOKEN ="8125526527:AAEd2PVmfb1CATBCxKHzgXec6DXNI5ZqB1o"
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
